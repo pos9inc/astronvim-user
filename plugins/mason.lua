f091959a79202afa78deaebb57ec23c6dd290f2e
pos9inc <pos9inc@gmail.com>
2023-06-11 17:24:04 +0900

    update mason/user.lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -3,13 +3,20 @@ return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "jsonls",
+        "yamlls",
+        "bashls",
+        "dockerls",
+        "docker_compose_language_service",
+        -- "gopls",
+        "helm_ls",
+        "marksman",
+        "terraformls"
+      },
+    },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
